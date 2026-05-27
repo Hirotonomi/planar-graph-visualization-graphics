223d9a0b7581b8484db24e54b79d6295c193bed5
Mateusz Kamieniecki <kammat006@wp.pl>
2026-05-27 22:07:50 +0200

    added all the io

--- app/src/main/java/graph/Vertex.java
+++ app/src/main/java/graph/Vertex.java
@@ -1,10 +1,11 @@
 package graph;
 
 public class Vertex {
-    int id;
-    float x;
-    float y;
-    public Vertex(int id, float x, float y) {
+    public int id;
+    public double x;
+    public double y;
+    
+    public Vertex(int id, double x, double y) {
         this.id = id;
         this.x = x;
         this.y = y;
