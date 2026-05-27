7c6854e7004297c1af7027036c0ab5f3f88eadeb
Mateusz Kamieniecki <kammat006@wp.pl>
2026-05-27 19:15:35 +0200

    forgor about edge name

--- app/src/main/java/graph/Edge.java
+++ app/src/main/java/graph/Edge.java
@@ -1,12 +1,16 @@
 package graph;
 
 public class Edge {
+    String name;
+
     Vertex from;
     Vertex to;
 
+
     double weight;
 
-    public Edge(Vertex from, Vertex to, double weight) {
+    public Edge(String name, Vertex from, Vertex to, double weight) {
+        this.name = name;
         this.from = from;
         this.to = to;
         this.weight = weight;
